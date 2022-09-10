706dfabadf8c1d8878025d9e2ec585bc4c40a846
charlotte <charlottediaz88@gmail.com>
2022-09-10 12:48:05 -0300

    triangles

---
+++ 06_triangles_two.py
@@ -0,0 +1,23 @@
+import time
+#Este programa ayuda a saber si podemos hacer un triàngulo
+#dadas tres longitudes
+
+print("hola.te voy a ayudar con eso de los triangulos")
+l1 = int(input("Ingresa la longitud uno\n"))
+l2 = int(input("Ingresa la longitud dos\n"))
+l3 = int(input("Ingresa la longitud tres\n"))
+
+print("Las longitudes entregadas hasta el numero son!")
+
+print(l1,l2,l3)
+
+def find_max(l1,l2,l3):
+    #...
+    return
+
+if(l1 <= l2+l3) and (l2 <= l1+l3) and (l3 <= l2+l1):
+    print("si es posible el triangulo con ",l1,l2,l3)
+else:
+    long_stick = find_max(l1,l2,l3)
+    other_sticks = other_sticks(l1,l2,l3)
+    print("No es posible formar un triàngulo:",l1,l2,l3,"No cumple la condiciòn")
