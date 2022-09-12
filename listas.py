ce631b8f7d388ead33f763aac78c64a9cdf9afed
charlotte <charlottediaz88@gmail.com>
2022-09-12 11:10:47 -0300

    cachipun

---
+++ listas.py
@@ -0,0 +1,23 @@
+
+students = [
+    'Charlotte',
+    'Aylin',
+    'Claudia',
+    'Elsy',
+    'Javiera',
+    'Maria soledad',
+    'Abel',
+    'Maryelin',
+    'Paola',
+    'Paola M',
+    'Yaritza',
+    'Silvia',
+    'Nicolas',
+    'Karen'
+]
+user_option = input("selecciona tu posiciòn \n")
+
+index = int(user_option)-1
+student = students[index]
+
+print(student)
